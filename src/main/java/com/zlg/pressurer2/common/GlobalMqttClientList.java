c990083918ac35bf4afaa70989b5c1715b7320aa
linyouru1 <linyouru@zlg.cn>
2023-05-16 16:36:14 +0800

    调整一下

---
+++ src/main/java/com/zlg/pressurer2/common/GlobalMqttClientList.java
@@ -0,0 +1,11 @@
+package com.zlg.pressurer2.common;
+
+import com.zlg.pressurer2.pojo.PressureMqttClient;
+
+import java.util.ArrayList;
+
+public class GlobalMqttClientList {
+
+    public static ArrayList<PressureMqttClient> mqttClientList;
+
+}
